a30c493acf14a04b7b2385b7c3a2b457eca80076
mseoa <0527sophie@naver.com>
2023-03-25 16:09:50 +0900

    [CHORE] index.js like comment 라우터 추가

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const likesRouter = require("./likes.route");
+const commentsRouter = require("./comments.route");
+
+// router.use('/',likesRouter);
+router.use('/posts/:postId/comments', commentsRouter);
+
 module.exports = router;
